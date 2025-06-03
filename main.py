aaa9dc967d22a8f298767757ef1d205d343c2335
AsrielNi <jasonplayer2010@gmail.com>
2025-06-04 00:45:16 +0800

    Hint: 參考 20250604_Log_01.txt

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
 from __future__ import annotations
 import tkinter
 import random
-
-class Banner:
-    def __init__(self, banner_name: str):
-        self._banner_name = banner_name
-        self.__draw_member_dict: dict[str, str] = dict()
-    def add_draw_member(self, name: str):
-        if self.__draw_member_dict.get(name) != None:
-            raise KeyError("該名稱已經登記了")
-        else:
-            self.__draw_member_dict[name] = name
-    def remove_draw_member(self, name: str):
-        if self.__draw_member_dict.get(name) == None:
-            raise KeyError("該名稱並未存在")
-        else:
-            del self.__draw_member_dict[name]
-    @property
-    def draw_member_dict(self):
-        return self.__draw_member_dict
+from inspect import signature, _empty
+from typing import Any
+from src.Banner import BaseBanner, BaseDrawMember, IDrawMember
 
 
 class AddDrawMemeberPage:
-    def __init__(self, parent_page: DrawPage, banner: Banner):
+    def __init__(self, parent_page: DrawPage, banner: BaseBanner[IDrawMember]):
         self._parent_page = parent_page
+        self._banner = banner
+        self._dm_sig = signature(self._banner.draw_member_type)
+        self._draw_member_opts: dict[str, tkinter.StringVar] = dict()
         self._interact_window = tkinter.Toplevel()
         self._interact_window.geometry("200x200+100+100")
         self._interact_window.protocol("WM_DELETE_WINDOW", self.__on_closing)
-        self._banner = banner
     def __on_closing(self):
         self._parent_page.add_button.config(state="normal")
         self._interact_window.destroy()
     def __confirm_member(self):
-        new_draw_member_name = self._sv.get()
-        if new_draw_member_name != "":
-            self._banner.add_draw_member(new_draw_member_name)
-            self._parent_page.refresh_draw_container()
+        value_pack_dict: dict[str, Any] = dict()
+        for dm_opt_name, sv in self._draw_member_opts.items():
+            input_value = sv.get()
+            if input_value == "":
+                raise ValueError("數值不能為空白。")
+            if self._dm_sig.parameters.get(dm_opt_name) == None:
+                raise KeyError("<Signature>物件並未記述有關此名稱({})的訊息。".format(dm_opt_name))
+            else:
+                expect_type = self._dm_sig.parameters[dm_opt_name].annotation
+                if issubclass(expect_type, int) == True:
+                    if input_value.isdigit() == False:
+                        raise ValueError("使用者輸入的值無法轉換成整數。")
+                    else:
+                        value_pack_dict[dm_opt_name] = int(input_value)
+                elif issubclass(expect_type, (str, _empty)) == True:
+                    value_pack_dict[dm_opt_name] = input_value
+                else:
+                    raise TypeError("沒有從<str>轉換成<{}>的方法。".format(expect_type))
+        new_draw_member = self._banner.draw_member_type(**value_pack_dict)
+        self._banner.add_draw_member(new_draw_member)
+        self.__clear_input()
+        self._parent_page.refresh_draw_container()
+    def __clear_input(self):
+        for sv in self._draw_member_opts.values():
+            sv.set("")
     def __cancel(self):
         self.__on_closing()
     def layout(self):
-        self._name_label = tkinter.Label(self._interact_window, text="名稱")
-        self._name_label.pack()
-        self._sv = tkinter.StringVar()
-        self._entry = tkinter.Entry(self._interact_window, textvariable=self._sv)
-        self._entry.pack()
+        self._draw_member_opts_frame = tkinter.Frame(self._interact_window)
+        self._draw_member_opts_frame.pack(side="top", fill="x")
+        for dm_opt in self._banner.draw_member_type._expose_opts():
+            pair_frame = tkinter.Frame(self._draw_member_opts_frame)
+            pair_frame.pack(side="top", fill="both")
+            dm_opt_label = tkinter.Label(pair_frame, text=dm_opt)
+            dm_opt_label.pack(side="left", fill="both")
+            dm_opt_sv = tkinter.StringVar()
+            dm_opt_entry = tkinter.Entry(pair_frame, textvariable=dm_opt_sv)
+            dm_opt_entry.pack(side="left", fill="both", expand=1)
+            self._draw_member_opts[dm_opt] = dm_opt_sv
         self._confirm_but = tkinter.Button(self._interact_window, text="確認", command=self.__confirm_member)
         self._confirm_but.pack()
         self._cancel_but = tkinter.Button(self._interact_window, text="取消", command=self.__cancel)
@@ -53,7 +67,7 @@ class AddDrawMemeberPage:
 
 
 class RemoveDrawMemeberPage:
-    def __init__(self, parent_page: DrawPage, banner: Banner):
+    def __init__(self, parent_page: DrawPage, banner: BaseBanner[BaseDrawMember]):
         self._parent_page = parent_page
         self._interact_window = tkinter.Toplevel()
         self._interact_window.geometry("200x200+100+100")
@@ -64,8 +78,8 @@ class RemoveDrawMemeberPage:
         self._interact_window.destroy()
     def __confirm_member(self):
         target_draw_member_name = self._sv.get()
-        if self._banner.draw_member_dict.get(target_draw_member_name) != None:
-            del self._banner.draw_member_dict[target_draw_member_name]
+        if self._banner.draw_members.get(target_draw_member_name) != None:
+            del self._banner.draw_members[target_draw_member_name]
             self._parent_page.refresh_draw_container()
     def __cancel(self):
         self.__on_closing()
@@ -87,7 +101,7 @@ class DrawPage:
     def __init__(self):
         self._top_window = tkinter.Tk()
         self._top_window.geometry("600x400+50+50")
-        self._banner = Banner("Test")
+        self._banner = BaseBanner[BaseDrawMember]("Test")
     def layout(self):
         self.proportion_frame = tkinter.Frame(self._top_window, relief="ridge", borderwidth=3)
         self.proportion_frame.place(relx=0, rely=0, relwidth=0.25, relheight=1)
@@ -133,22 +147,22 @@ class DrawPage:
     def start(self):
         self._top_window.mainloop()
     ######## 行為 ########
-    def open_add_draw_member_page(self, banner: Banner):
+    def open_add_draw_member_page(self, banner: BaseBanner[BaseDrawMember]):
         self.add_button.config(state="disabled")
         add_page = AddDrawMemeberPage(self, banner)
         add_page.render()
-    def open_remove_draw_member_page(self, banner: Banner):
+    def open_remove_draw_member_page(self, banner: BaseBanner[BaseDrawMember]):
         self.add_button.config(state="disabled")
         add_page = RemoveDrawMemeberPage(self, banner)
         add_page.render()
     def refresh_draw_container(self):
         for widget in self.proportion_frame.winfo_children():
             widget.destroy()
-        for draw_member_name in self._banner.draw_member_dict.keys():
+        for draw_member_name in self._banner.draw_members.keys():
             new_lebel = tkinter.Label(self.proportion_frame, text=draw_member_name)
             new_lebel.pack()
-    def draw(self, banner: Banner):
-        population = [draw_m for draw_m in banner.draw_member_dict.keys()]
+    def draw(self, banner: BaseBanner[BaseDrawMember]):
+        population = [draw_m for draw_m in banner.draw_members.keys()]
         result_member = random.choice(population)
         self.result_label["text"] = result_member
 
---
+++ src/Banner.py
@@ -0,0 +1,97 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic, Any, get_args
+
+
+class IDrawMember(ABC):
+    @classmethod
+    @abstractmethod
+    def _expose_opts(cls) -> tuple[str, ...]:
+        raise NotImplementedError()
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        raise NotImplementedError()
+    @property
+    @abstractmethod
+    def proportion(self) -> int:
+        raise NotImplementedError()
+    
+DM = TypeVar("DM", bound=IDrawMember)
+
+class BaseDrawMember(IDrawMember):
+    _EXPOSE_OPTS = ("name", "proportion") # 公開參數名稱必須和建構式的參數名稱完全一樣
+    @classmethod
+    def _expose_opts(cls):
+        temp_check_dict = dict()
+        for opts in cls._EXPOSE_OPTS:
+            if temp_check_dict.get(opts) != None:
+                raise KeyError("抽選對象的公開參數名稱({})重複了。".format(opts))
+            else:
+                temp_check_dict[opts] = True
+        del temp_check_dict
+        return cls._EXPOSE_OPTS
+    def __init__(self, name: str, proportion: int = 1):
+        self._name = name
+        self._proportion = proportion
+    @property
+    def name(self):
+        return self._name
+    @property
+    def proportion(self):
+        return self._proportion
+
+
+class IBanner(ABC):
+    @abstractmethod
+    def __init__(self, banner_name: str):
+        raise NotImplementedError()
+    @abstractmethod
+    def add_draw_member(self, draw_member: Any):
+        raise NotImplementedError()
+    @abstractmethod
+    def remove_draw_member(self, draw_member: Any):
+        raise NotImplementedError()
+    @property
+    @abstractmethod
+    def banner_name(self) -> str:
+        raise NotImplementedError()
+    @property
+    @abstractmethod
+    def draw_members(self) -> dict[str, Any]:
+        raise NotImplementedError()
+    @property
+    @abstractmethod
+    def draw_member_type(self) -> type[Any]:
+        raise NotImplementedError()
+
+
+class BaseBanner(IBanner, Generic[DM]):
+    def __init__(self, banner_name: str):
+        self._banner_name = banner_name
+        self._draw_members: dict[str, DM] = dict()
+    def add_draw_member(self, draw_member: DM):
+        if self._draw_members.get(draw_member.name) != None:
+            raise KeyError("該抽獎對象的名稱({})重複了".format(draw_member.name))
+        else:
+            self._draw_members[draw_member.name] = draw_member
+    def remove_draw_member(self, draw_member_name: str):
+        if self._draw_members.get(draw_member_name) == None:
+            raise KeyError("該名稱({})並不存在。".format(draw_member_name))
+        else:
+            del self._draw_members[draw_member_name]
+    @property
+    def banner_name(self) -> str:
+        return self._banner_name
+    @property
+    def draw_members(self) -> dict[str, DM]:
+        return self._draw_members
+    @property
+    def draw_member_type(self) -> type[DM]:
+        if hasattr(self, "__orig_class__") == True:
+            g1_type = get_args(self.__orig_class__)[0]
+            if issubclass(g1_type, IDrawMember) == True:
+                return g1_type
+            else:
+                raise TypeError("給定的類別({})必須是<IDrawMember>的子類。".format(g1_type.__name__))
+        else:
+            raise TypeError("未指定泛型類別，請指定一個<IDrawMember>的子類。 -> Example: {}[IDrawMember]()".format(self.__class__.__name__))
